d896eca2110ee72982de6888235a926e2cb050a8
msfarookhy <msfarookhy@gmail.com>
2018-09-19 23:28:22 +0530

    sum Question soved

---
+++ DataStructure/Q4.js
@@ -0,0 +1,8 @@
+function popShift(arr) {
+    let popped=arr.pop(); // change this line
+    let shifted=arr.shift(2,3); // change this line
+    return [shifted, popped];
+  }
+  
+  // do not change code below this line
+  console.log(popShift(['challenge', 'is', 'not', 'complete']));
---
+++ DataStructure/Q7.js
@@ -0,0 +1,8 @@
+function forecast(arr) {
+    // change code below this line
+    var a =arr.slice(2,4);
+    return a;
+  }
+  
+  // do not change code below this line
+  console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
