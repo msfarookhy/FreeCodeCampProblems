ca952966e59415c6f61b7e5d8beb370352e1a8be
msfarookhy <msfarookhy@gmail.com>
2018-09-21 16:45:15 +0530

    DataStructures

--- DataStructure/Q19.js
+++ DataStructure/Q19.js
@@ -19,35 +19,8 @@ let users = {
   
   function getArrayOfUsers(obj) {
     // change code below this line
-  return Object.keys(users)
+    return Object.keys(users);
     // change code above this line
   }
   
-  console.log(getArrayOfUsers(users));let user = {
-    name: 'Kenneth',
-    age: 28,
-    data: {
-      username: 'kennethCodesAllDay',
-      joinDate: 'March 26, 2016',
-      organization: 'freeCodeCamp',
-      friends: [
-        'Sam',
-        'Kira',
-        'Tomo'
-      ],
-      location: {
-        city: 'San Francisco',
-        state: 'CA',
-        country: 'USA'
-      }
-    }
-  };
-  
-  function addFriend(userObj, friend) {
-    // change code below this line  
-   userObj.data.friends.push(friend);
-    return userObj.data.friends;
-    // change code above this line
-  }
-  
-  console.log(addFriend(user, 'Pete'));
+  console.log(getArrayOfUsers(users));
